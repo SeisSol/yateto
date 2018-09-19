bbbbd6bdaa2cc557876627cc8942a38c37908371
Carsten Uphoff <uphoff@in.tum.de>
2018-09-19 12:36:26 +0200

    Extended InitTools

--- include/yateto/InitTools.h
+++ include/yateto/InitTools.h
@@ -5,25 +5,33 @@
 
 namespace yateto {
   template<class T>
-  constexpr unsigned numFamilyMembers() {
+  constexpr size_t numFamilyMembers() {
     return sizeof(T::Size) / sizeof(T::Size[0]);
   }
-  template<class T>
-  constexpr unsigned computeFamilySize(unsigned n = numFamilyMembers<T>()) {
-    return n == 0 ? 0 : T::Size[n-1] + computeFamilySize<T>(n-1);
+  
+  template<typename int_t>
+  constexpr size_t alignedUpper(int_t size, size_t alignment) {    
+    return size + (alignment - size % alignment) % alignment;
   }
   
-  template<class T, typename float_t>
-  void setFamilyPtr(float_t* ptrs[]) {
-    
+  template<typename float_t>
+  constexpr size_t alignedReals(size_t alignment) {
+    return alignment / sizeof(float_t);
+  }
+
+  template<class T>
+  constexpr size_t computeFamilySize(size_t alignedReals = 1, size_t n = numFamilyMembers<T>()) {
+    return n == 0 ? 0 : alignedUpper(T::Size[n-1], alignedReals) + computeFamilySize<T>(alignedReals, n-1);
   }
 
   template<class T, typename float_t>
-  void copyFamilyToMemAndSetPtr(float_t*& mem, float_t* ptrs[]) {
-    unsigned n = sizeof(T::Size) / sizeof(T::Size[0]);
-    for (unsigned i = 0; i < n; ++i) {
+  void copyFamilyToMemAndSetPtr(float_t*& mem, float_t* ptrs[], size_t alignment = 1) {
+    size_t n = sizeof(T::Size) / sizeof(T::Size[0]);
+    for (size_t i = 0; i < n; ++i) {
       ptrs[i] = mem;
       mem = std::copy(T::Values[i], T::Values[i] + T::Size[i], mem);
+      mem += (alignedUpper(reinterpret_cast<uintptr_t>(mem), alignment) - reinterpret_cast<uintptr_t>(mem)) / sizeof(float_t);
+      assert(reinterpret_cast<uintptr_t>(mem) % alignment == 0);
     }
   }
 }

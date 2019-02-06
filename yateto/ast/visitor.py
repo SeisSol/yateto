dd6311df2fc2bec6f6e201800260b4f97f559c7d
Carsten Uphoff <uphoff@in.tum.de>
2019-02-06 18:18:57 +0100

    Fix eqspp pdf print

--- yateto/ast/visitor.py
+++ yateto/ast/visitor.py
@@ -1,4 +1,5 @@
-from numpy import ndindex
+from numpy import ndindex, arange
+import math
 import collections
 import itertools
 import re
@@ -151,41 +152,57 @@ class PrintEquivalentSparsityPatterns(Visitor):
     if not (pltSpec and colorsSpec):
       raise NotImplementedError('Missing modules matplotlib')
     self._directory = directory
-    self._prefix = ''
     self._cmap = colors.ListedColormap(['white', 'black'])
     self._norm = colors.BoundaryNorm([0.0, 0.5, 1.0], 2, clip=True)
   
   def generic_visit(self, node):
-    oldPrefix = self._prefix
+    #~ oldPrefix = self._prefix
+    #~ nameFun = getattr(node, 'name', None)
+    #~ name = re.sub('\]', '', re.sub('\[', '_', str(node)))
+    #~ basePrefix = '{}--{}'.format(self._prefix, name) if len(self._prefix) > 0 else name
+    #~ counter = 0
+    #~ for child in node:
+      #~ self._prefix = basePrefix + '.' + str(counter)
+      #~ self.visit(child)
+      #~ counter = counter + 1
+    #~ fileName = os.path.join(self._directory, basePrefix + '.pdf')
     nameFun = getattr(node, 'name', None)
-    name = re.sub('\]', '', re.sub('\[', '_', str(node)))
-    basePrefix = '{}--{}'.format(self._prefix, name) if len(self._prefix) > 0 else name
+    name = nameFun() if nameFun else '_result'
+    baseDirectory = self._directory
     counter = 0
     for child in node:
-      self._prefix = basePrefix + '.' + str(counter)
+      if len(child) > 0:
+        self._directory = os.path.join(baseDirectory, '{}_{}'.format(counter, type(child).__name__))
+      else:
+        self._directory = baseDirectory
+      os.makedirs(self._directory, exist_ok=True)
       self.visit(child)
       counter = counter + 1
-    fileName = os.path.join(self._directory, basePrefix + '.pdf')
+    fileName = os.path.join(baseDirectory, name + '.pdf')
     eqspp = node.eqspp()
     nSubplots = 1
     for dim in range(2, eqspp.ndim):
       nSubplots *= eqspp.shape[dim]
-    fig, axs = plt.subplots(nSubplots)
+    nrows = math.ceil(math.sqrt(nSubplots))
+    ncols = math.ceil(nSubplots / nrows)
+    fig, axs = plt.subplots(nrows, ncols)
+    if ncols > 1:
+      axs = [y for x in axs for y in x]
+    pattern = eqspp.as_ndarray()
     if nSubplots == 1:
-      axs.imshow(eqspp.astype(bool), cmap=self._cmap, norm=self._norm)
+      axs.imshow(pattern.astype(bool), cmap=self._cmap, norm=self._norm)
     else:
       nSubplot = 0
       for index in ndindex(*list(eqspp.shape)[2:]):
-        sl = eqspp[(slice(None, None), slice(None, None)) + index]
+        sl = pattern[(slice(None, None), slice(None, None)) + index]
         axs[nSubplot].imshow(sl.astype(bool), cmap=self._cmap, norm=self._norm)
         axs[nSubplot].set_title('(:,:,{})'.format(','.join([str(i) for i in index])), y=1.2)
         nSubplot = nSubplot + 1
     #plt.setp(axs, xticks=arange(eqspp.shape[1]), yticks=arange(eqspp.shape[0]))
-    fig.set_size_inches(nSubplots*eqspp.shape[0] / 3.0, eqspp.shape[1] / 3.0)
     fig.tight_layout()
     fig.savefig(fileName, bbox_inches='tight')
     plt.close()
-    self._prefix = oldPrefix
+    self._directory = baseDirectory
 
 
 class FindPrefetchCapabilities(Visitor):

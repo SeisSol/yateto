27c0ff470fa33e3c2989bb64983ecddb1ded4440
ravil <ravil.dorozhinskii@tum.de>
2019-07-22 11:26:03 +0200

    input.py and type.py were partially documented

--- yateto/input.py
+++ yateto/input.py
@@ -6,99 +6,291 @@ from .memory import CSCMemoryLayout, DenseMemoryLayout
 from . import aspp
 
 import importlib.util
+
+
 lxmlSpec = importlib.util.find_spec('lxml')
 etreeSpec = importlib.util.find_spec('lxml.etree') if lxmlSpec else None
+
+
 if etreeSpec:
   etree = etreeSpec.loader.load_module()
 else:
   etree = importlib.util.find_spec('xml.etree.ElementTree').loader.load_module()
 
+
 def __createCollection(matrices):
-  maxIndex = dict()
+  """Creates a collection (a table of tables) from a given table of tensors.
+
+  A collection holds tensors in a hierarchical
+  i.e. base names followed by tensor groups
+
+  Args:
+    matrices (Dict[str, Tensor]): a table of tensors
+
+  Returns:
+    Collection (Dict[str, Dict[int, Tensor]]): a table of tables
+
+  Raises:
+    ValueError: if a matrix tensor name is illegal according yateto specification.
+                See the definition of type.Tensor class.
+  """
+
   collection = Collection()
+
   for name, matrix in matrices.items():
+
     if not Tensor.isValidName(name):
-      raise ValueError('Illegal matrix name', name, 'in', xmlFile)
-    baseName = Tensor.getBaseName(name)
+      raise ValueError('Illegal matrix name: {}'.format(name))
+
+    base_name = Tensor.getBaseName(name)
     group = Collection.group(name)
+
     if group is tuple():
-      collection[baseName] = matrix
+      collection[base_name] = matrix
+
     else:
-      if baseName in collection:
-        collection[baseName][group] = matrix
+      if base_name in collection:
+        collection[base_name][group] = matrix
+
       else:
-        collection[baseName] = {group: matrix}
+        collection[base_name] = {group: matrix}
 
   return collection
 
+
 def __transposeMatrix(matrix):
-  matrixT = dict()
-  for entry,value in matrix.items():
-    matrixT[(entry[1], entry[0])] = value
-  return matrixT
+  """Transposes a matrix
+
+  Args:
+    matrix (Dict[Tuple[int, int], float]): sparse matrix specification as a hash table
+
+  Returns:
+    Dict[Tuple[int, int], float]: transpose matrix description
+  """
+
+  transposed_matrix = dict()
+
+  for entry, value in matrix.items():
+    transposed_matrix[(entry[1], entry[0])] = value
+
+  return transposed_matrix
+
 
 def __processMatrix(name, rows, columns, entries, clones, transpose, alignStride):
+  """Creates a tensor (tensors) using passed parameters i.e. a matrix description
+
+  Parameters are retrieved while processing an xml matrix-file. Using parameter 'clones',
+  the function allows the user to either change the target name of a tensor or to generate
+  multiple tensors using a given matrix description.
+
+  Args:
+    name (str): matrix name (which was retrived processing an xml file)
+    rows (int): number of rows in a matrix
+    columns (int): number of columns in  a matrix
+    entries (List[Tuple]): a list of all entries retrieved from a matrix specified an xml file.
+                           The list essentially is a coordinate sparse matrix form.
+    clones (Dict[str, List[str]]): a table of target tensor names
+
+    transpose (function): function takes a name of a matrix as an input and
+                          returns a boolean flag which specifies whether or not
+                          yateto has to transpose a matrix
+
+    alignStride (function): function takes a name of a matrix as an input and
+                            returns a boolean flag which specifies whether or not
+                            yateto has to align a given matrix layout for vectorization
+
+  Returns:
+    Dict[str, Tensor]: a table of tensors
+  """
+
+  # create an empty hash table
   matrix = dict()
+
+  # traverse a list of matrix entries and generate a matrix description
+  # as a hash table
   for entry in entries:
-    row = int(entry[0])-1
-    col = int(entry[1])-1
+
+    # adjust row and column numbers
+    row = int(entry[0]) - 1
+    col = int(entry[1]) - 1
+
+    # allocate a matrix element inside of a table
     matrix[(row, col)] = entry[2]
 
+
+  # allocate an empty hash table to hold tensors (matrices) which are going to be generated
+  # using the matrix description
   matrices = dict()
+
+  # create target tensors names
   names = clones[name] if name in clones else [name]
+
+  # generate tensors using description of a give matrix
   for name in names:
+
+    # compute a shape of a tensor
     shape = (columns, rows) if transpose(name) else (rows, columns)
     if shape[1] == 1:
       shape = (shape[0],)
+
+    # transpose matrix if it is needed
     mtx = __transposeMatrix(matrix) if transpose(name) else matrix
+
+    # adjust layout description in case if a given matrix is a vector
     if len(shape) == 1:
-      mtx = {(i[0],): val for i,val in mtx.items()}
-    matrices[name] = Tensor(name, shape, mtx, alignStride=alignStride(name))
+      mtx = {(i[0],): val for i, val in mtx.items()}
+
+    # Create an tensor(matrix) using the matrix description and append the hash table
+    matrices[name] = Tensor(name=name,
+                            shape=shape,
+                            spp=mtx,
+                            alignStride=alignStride(name))
   return matrices
 
+
 def __complain(child):
+  """Raises an exception in case of a violation of the xml-matrix format specification
+
+  Args:
+    child: a node of an xml parse tree
+
+  Returns:
+
+  """
   raise ValueError('Unknown tag ' + child.tag)
 
-def parseXMLMatrixFile(xmlFile, clones=dict(), transpose=lambda name: False, alignStride=lambda name: False):
-  tree = etree.parse(xmlFile)
-  root = tree.getroot()
-  
-  matrices = dict()
-  
-  for node in root:
-    if node.tag == 'matrix':
-      name = node.get('name')
-      rows = int( node.get('rows') )
-      columns = int( node.get('columns') )
 
+def parseXMLMatrixFile(xml_file,
+                       clones=dict(),
+                       transpose=lambda name: False,
+                       align_stride=lambda name: False):
+  """Generates a table of tensors from an xml file containing matrix(tensors) structure and data
+
+  The function reads an xml file and retrieve information about matrices specified in it.
+  All matrices are given in sparse coordinate form assuming that the first matrix entry
+  has indices 1,1 (instead of 0,0).
+
+  Args:
+    xml_file (str): path to a file
+    clones (Dict[str, List[str]]): a table of target tensor names
+    transpose (function): function takes a name of a matrix as an input and
+                          returns a boolean value which specifies whether or not
+                          a matrix has to be transposed
+
+    align_stride (function): function takes a name of a matrix as an input and
+                             returns a boolean flag which specifies whether or not
+                             a given matrix layout has to be aligned for vectorization
+
+  Returns:
+    Collection: a table of tensors arranged by their base names and tensor groups
+  """
+
+  # parse a give xml file and build a parse tree
+  tree = etree.parse(xml_file)
+  root = tree.getroot()
+
+
+  # allocate a dictionary to hold all matrices specified in the file
+  # after processing a given xml file
+  matrices = dict()
+
+  # traverse a parse tree
+  for node in root:
+
+    # check whether a node of a tree describes a matrix.
+    # Raise an exception if it is not a case
+    if node.tag == 'matrix':
+
+      # retrive name and size of a matrix
+      name = node.get('name')
+      rows = int(node.get('rows'))
+      columns = int(node.get('columns'))
+
+      # allocate a list to hold entries of a matrix specified in a node of a given xml file
       entries = list()
+
+      # traverse children of a matrix node and collect data
       for child in node:
+
+        # Abort execution of a node child is not denoted as a entry i.e. entry of matrix
         if child.tag == 'entry':
+
+          # collect data i.e. row and column numbers as well as a matrix element value
           row = int(child.get('row'))
           col = int(child.get('column'))
           entry = child.get('value', True)
-          entries.append((row,col,entry))
+
+          # put all found matrix elements to a list
+          entries.append((row, col, entry))
         else:
           __complain(child)
 
-      matrices.update( __processMatrix(name, rows, columns, entries, clones, transpose, alignStride) )
+      # generate a tensor (tensors) using retrieved matrix description
+      tensors = __processMatrix(name, rows, columns, entries, clones, transpose, align_stride)
+
+      # append the dictionary with a generated tensor (tensors)
+      matrices.update(tensors)
+
     else:
       __complain(node)
 
-  return __createCollection(matrices)
+  try:
+    collection = __createCollection(matrices)
+  except ValueError as error:
+    print("ERROR: {} in {}".format(error, xml_file))
+    raise
 
-def parseJSONMatrixFile(jsonFile, clones=dict(), transpose=lambda name: False, alignStride=lambda name: False):
+  return collection
+
+
+def parseJSONMatrixFile(json_file,
+                        clones=dict(),
+                        transpose=lambda name: False,
+                        alignStride=lambda name: False):
+  """Generates a table of tensors from an xml file containing matrix(tensors) structure and data
+
+  The function reads an xml file and retrieve information about matrices specified in it.
+  All matrices are given in sparse coordinate form assuming that the first matrix entry
+  has indices 1,1 (instead of 0,0).
+
+
+  Args:
+    json_file: path to a file
+    clones (Dict[str, List[str]]): a table of target tensor names
+    transpose (function): function takes a name of a matrix as an input and
+                          returns a boolean value which specifies whether or not
+                          a matrix has to be transposed
+
+    align_stride (function): function takes a name of a matrix as an input and
+                             returns a boolean flag which specifies whether or not
+                             a given matrix layout has to be aligned for vectorization
+
+  Returns:
+    Collection: a table of tensors arranged by their base names and tensor groups
+  """
   matrices = dict()
 
-  with open(jsonFile) as j:
-    content = json.load(j)
-    for m in content:
-      entries = m['entries']
+  with open(json_file) as file:
+    content = json.load(file)
+
+    for matrix in content:
+      entries = matrix['entries']
+
       if len(next(iter(entries))) == 2:
         entries = [(entry[0], entry[1], True) for entry in entries]
-      matrices.update( __processMatrix(m['name'], m['rows'], m['columns'], entries, clones, transpose, alignStride) )
+
+      tensors = __processMatrix(matrix['name'],
+                                matrix['rows'],
+                                matrix['columns'],
+                                entries,
+                                clones,
+                                transpose,
+                                alignStride)
+
+      matrices.update(tensors)
 
   return __createCollection(matrices)
+
 
 def memoryLayoutFromFile(xmlFile, db, clones):
   tree = etree.parse(xmlFile)
@@ -110,23 +302,31 @@ def memoryLayoutFromFile(xmlFile, db, clones):
     groupName = group.get('name')
     noMutualSparsityPattern = group.get('noMutualSparsityPattern', '').lower() in strtobool
     groups[groupName] = list()
+
     for matrix in group:
       if matrix.tag == 'matrix':
         matrixName = matrix.get('name')
+
         if not db.containsName(matrixName):
           raise ValueError('Unrecognized matrix name ' + matrixName)
+
         if len(groups[groupName]) > 0:
           lastMatrixInGroup = groups[groupName][-1]
+
           if db.byName(lastMatrixInGroup).shape() != db.byName(matrixName).shape():
             raise ValueError('Matrix {} cannot be in the same group as matrix {} due to different shapes.'.format(matrixName, lastMatrixInGroup))
+
         groups[groupName].append( matrixName )
+
       else:
         __complain(group)
+
     # equalize sparsity pattern
     if not noMutualSparsityPattern:
       spp = None
       for matrix in groups[groupName]:
         spp = aspp.add(spp, db.byName(matrix).spp()) if spp is not None else db.byName(matrix).spp()
+
       for matrix in groups[groupName]:
         db.byName(matrix).setGroupSpp(spp)
 
--- yateto/type.py
+++ yateto/type.py
@@ -5,44 +5,99 @@ from numpy import ndarray, zeros
 from .memory import DenseMemoryLayout
 from . import aspp
 
+
 class AbstractType(object):
   @classmethod
   def isValidName(cls, name):
+    """Checks whether a name is valid for a particular subclass
+
+    NOTE: it is a class methods
+
+    Args:
+      name (str): a name
+
+    Returns:
+      bool: True if it is valid. Otherwise, False
+    """
     return re.match(cls.VALID_NAME, name) is not None
-  
+
+
   def name(self):
+    """Returns a name of an instance
+
+    NOTE: every subclass of AbstractType must have a member called _name
+    Refactoring: get_name
+
+    Returns:
+      str: name of a subclass instance
+    """
     return self._name
 
+
 class Scalar(AbstractType):
+
+  # VALID_NAME is a sequence of any alphabet character
+  #	  followed by any number of word characters
   VALID_NAME = r'^[a-zA-Z]\w*$'
   
   def __init__(self, name):
+    """
+    Args:
+      name (str): a name of an instance of the class
+
+    Raises:
+      ValueError: if name is not valid according to the specification
+    """
+
     if not self.isValidName(name):
-      raise ValueError('Scalar name invalid (must match regexp {}): {}'.format(self.VALID_NAME, name))
+      raise ValueError('Scalar name invalid (must match regexp {}): {}'.format(self.VALID_NAME,
+                                                                               name))
 
     self._name = name
   
   def __str__(self):
     return self._name
 
+
 class Tensor(AbstractType):
+
+  # BASE_NAME is a sequence of any alphabet character
+  #	  followed by any number of word characters
   BASE_NAME = r'[a-zA-Z]\w*'
+
+
+  # GROUP_INDEX is a group containing either 0
+  #   or a digit from 1 to 9 followed by any number of digits
   GROUP_INDEX = r'(0|[1-9]\d*)'
+
+
+  # GROUP_INDICES is the open parentheses followed by
+  #     a group containing GROUP_INDEX followed by
+  #		      many repeats of the second group
+  #			        containing comma "," followd by
+  #				          GROUP_INDEX
+  # followed by a closed the parentheses
   GROUP_INDICES = r'\(({0}(,{0})*)\)'.format(GROUP_INDEX)
+
+
+  # VALID_NAME is BASE_NAME optionally ends with
+  # a group containing GROUP_INDICES
   VALID_NAME = r'^{}({})?$'.format(BASE_NAME, GROUP_INDICES)
 
+
   def __init__(self, name, shape, spp=None, memoryLayoutClass=DenseMemoryLayout, alignStride=False):
+    """TODO
+
+    Args:
+      name (str): tensor name. The name string must match a regex defined by VALID_NAME
+      shape (Tuple[int, int]): shape of a tensor
+      spp (TODO): sparsity pattern of a tensor
+      memoryLayoutClass (Type[MemoryLayout]):
+      alignStride (bool): a flag which tell yateto whether to align a matrix memory layout
+                          for vectorization
     """
-    Parameters
-    ----------
-    name : str
-        a text string must match a regex defined by VALID_NAME
-    shape : tuple
-    spp : an instance of a derived spp class
-    memoryLayoutClass : an instance of a derived MemoryLayout class
-    alignStride : bool
-        TODO: complete description
-    """
+
+    # check whether the input specified by the user is correct
     if not isinstance(shape, tuple):
       raise ValueError('shape must be a tuple')
     
@@ -52,10 +107,12 @@ class Tensor(AbstractType):
     if not self.isValidName(name):
       raise ValueError('Tensor name invalid (must match regexp {}): {}'.format(self.VALID_NAME, name))
 
+    # init data members of an instance
     self._name = name
     self._shape = shape
     self._values = None
-    
+
+
     if spp is not None:
       if isinstance(spp, dict):
         if not isinstance(next(iter(spp.values())), bool):
@@ -74,14 +131,17 @@ class Tensor(AbstractType):
         raise ValueError(name, 'Matrix values must be given as dictionary (e.g. {(1,2,3): 2.0} or as numpy.ndarray.')
     else:
       # if the user din't specify a certain sparsity pattern, then
-      # create a dense sparsity pattern object with give shape from assp module
+      # create a dense sparsity pattern object with a given shape from assp module
       self._spp = aspp.dense(shape)
     self._groupSpp = self._spp
-    
+
+    # set memory layout for a tensor
     self.setMemoryLayout(memoryLayoutClass, alignStride)
+
 
   def setMemoryLayout(self, memoryLayoutClass, alignStride=False):
     self._memoryLayout = memoryLayoutClass.fromSpp(self._groupSpp, alignStride=alignStride)
+
 
   def _setSparsityPattern(self, spp, setOnlyGroupSpp=False):
     if spp.shape != self._shape:
@@ -91,9 +151,11 @@ class Tensor(AbstractType):
       self._spp = spp
     self._groupSpp = spp
 
+
   def setGroupSpp(self, spp):
     self._setSparsityPattern(spp, setOnlyGroupSpp=True)
     self.setMemoryLayout(self._memoryLayout.__class__, alignStride=self._memoryLayout.alignedStride())
+
 
   def __getitem__(self, indexNames: str) -> IndexedTensor:
     """
@@ -115,71 +177,289 @@ class Tensor(AbstractType):
     """
     return self._shape
 
+
   def memoryLayout(self):
     return self._memoryLayout
-  
+
+
   @classmethod
   def getBaseName(cls, name: str):
+    """Extracts the original name of the tensor discarding a tensor group if any.
+
+     The original tensor name is all the characters before the tensor group
+     which is inside parentheses.
+
+     NOTE: The function is static i.e. the user can use it
+     without creating an instance of the class.
+
+     Args:
+       name (str): a full name of a tensor including a tensor group if any
+
+     Returns:
+       str: a base name of a tensor
+
+     Examples:
+       >>> name = "aTensor(1,5,12)"
+       >>> base_name = Tensor.getGroup(name)
+       >>> print(base_name)
+       'aTensor'
+     """
     return re.match(cls.BASE_NAME, name).group(0)
-  
+
+
   def baseName(self):
+    """Extracts the original name of the tensor discarding a tensor group if any.
+
+    The original tensor name is all the characters before the tensor group.
+    which is inside parentheses.
+
+    NOTE: The function is internal.
+
+    Returns:
+      str: a base name of a tensor
+
+    Examples:
+      >>> name = "aTensor(1,5,12)"
+      >>> base_name = Tensor.getGroup(name)
+      >>> print(base_name)
+      'aTensor'
+    """
     return self.getBaseName(self._name)
+
 
   @classmethod
   def getGroup(cls, name):
-    gis = re.search(cls.GROUP_INDICES, name)
-    if gis:
-      return tuple(int(gi) for gi in re.split(',', gis.group(1)))
+    """Extracts a tensor group encoded as a set of integers inside of a tensor name
+
+    A tensor group is encoded inside of parentheses of a tensor name.
+    NOTE: The function is static i.e. the user can use it
+    without creating an instance of the class.
+
+    Args:
+      name (str): a tensor name
+
+    Returns:
+      Tuple[int]: a tensor group based on a tensor name
+
+    Examples:
+      >>> name = "aTensor"
+      >>> group = Tensor.getGroup(name)
+      >>> print(group)
+      ()
+      >>> name = "aTensor(1,5,12)"
+      >>> group = Tensor.getGroup(name)
+      >>> print(group)
+      (1, 5, 12)
+    """
+    
+    matches = re.search(cls.GROUP_INDICES, name)
+    if matches:
+      # return a tensor group if the name contains any
+      return tuple(int(match) for match in re.split(',', matches.group(1)))
+
+    # otherwise, return an empty tuple
     return tuple()
 
+
   def group(self):
+    """Extracts a tensor group encoded as a set of integers inside of a tensor name
+
+    NOTE: The function is internal.
+
+    Returns:
+      Tuple[int]: a tensor group based on a tensor name
+    """
     return self.getGroup(self._name)
-  
+
+
   def spp(self, groupSpp=True):
     return self._groupSpp if groupSpp else self._spp
-  
+
+
   def values(self):
     return self._values
-  
+
+
   def __eq__(self, other):
     equal = self._name == other._name
     if equal:
       assert self._shape == other._shape and aspp.array_equal(self._spp, other._spp) and self._memoryLayout == other._memoryLayout
     return equal
-  
+
+
   def __hash__(self):
     return hash(self._name)
-  
+
+
   def __str__(self):
     return '{}: {}'.format(self._name, self._shape)
 
+
 class Collection(object):
+  """The class represents a table of dictionaries which holds tensors according their
+  names and tensor groups
+
+  Each key of a class instance is a string. Each string corresponds to
+  a sub-table which holds a table of tensors which belong to the same "class".
+  Each key of a sub-table is an integer whereas its value is an instance of the Tensor class.
+
+  NOTE: self is an instance of class Dict i.e. (<class 'dict'>)
+
+  Examples:
+    >>> names = ['A_00', 'A_01', 'B_0', 'C']
+    >>> shapes = [(4,4), (3,4), (3,2), (3,2)]
+    >>> tensors = {}
+    >>> for name, shape in zip(names, shapes):
+    ...     tensors[name] = Tensor(name, shape)
+    ...
+    >>> from yateto.input import __createCollection
+    >>> collection = __createCollection(Tensors)
+
+  tensor = {'A': {0: <yateto.type.Tensor object at 'A_00'>}, {1: <yateto.type.Tensor object at 'A_01'>}, \
+            'B': {0: <yateto.type.Tensor object at 'B_0'>}, \
+            'C': <yateto.type.Tensor object at 'C'>}
+  """
+
   def update(self, collection):
+    """Concatenates two tables together
+
+    Args:
+      collection (Collection): another table of type Collection
+
+    Returns:
+      Collection: augmented collection(table)
+    """
     self.__dict__.update(collection.__dict__)
 
+
   def __getitem__(self, key):
+    """Returns a reference to a tensor stored in the collection
+
+    Args:
+      key (str): a full tensor name
+
+    Returns:
+      Tensor: a reference to a tensor which corresponds to a given name
+    """
     return self.__dict__[key]
-  
+
+
   def __setitem__(self, key, value):
+    """Inserts a new element to a collection
+
+    Args:
+      key (str): a base name of a tensor
+      value (Dict[int, Tensor]): another tensor table
+    """
     self.__dict__[key] = value
 
+
   def __contains__(self, key):
+    """Check whether collection(table) contains an entry
+
+    Args:
+      key (str): a base name of a tensor
+
+    Returns:
+      bool: True if a collection contains the key. Otherwise, False
+    """
     return key in self.__dict__
-  
+
+
   @classmethod
   def group(cls, name):
+    """Returns a group of a tensor embedded inside of a full tensor name
+
+    The algorithm is based on a regular expression defined in the Tensor class
+    Suggested method name: extract_tensor_group
+
+    Args:
+      name (str): a name of a tensor
+
+    Returns:
+      [Tuple[int], int]: a tensor group based on a tensor name
+
+    Examples:
+      >>> name = "aTensor(1,2,3)"
+      >>> Collection.group(name)
+      (1, 2, 3)
+
+      >>> name = "aTensor(3)"
+      >>> Collection.group(name)
+      3
+
+
+      >>> name = "aTensor"
+      >>> Collection.group(name)
+      ()
+    """
     group = Tensor.getGroup(name)
     return group if len(group) != 1 else group[0]
 
+
   def byName(self, name):
-    baseName = Tensor.getBaseName(name)
+    """Returns a tensor instance if it exists inside ot the table.
+
+    Refactoring: get_tensor_by_name.
+
+    Args:
+      name (str): a tensor name
+
+    Returns:
+      Tensor: a tensor
+
+    Examples:
+      >>> names = ['A_00', 'A_01', 'B_0', 'C']
+      >>> shapes = [(4,4), (3,4), (3,2), (3,2)]
+      >>> tensors = {}
+      >>> for name, shape in zip(names, shapes):
+      ...     tensors[name] = Tensor(name, shape)
+      ...
+      >>> from yateto.input import __createCollection
+      >>> collection = __createCollection(Tensors)
+      >>> collection.byName('A_00')
+      <yateto.type.Tensor object at 0x7fcab0ed8588>
+      >>> collection.byName('C')
+      <yateto.type.Tensor object at 0x7fcab0af5470>
+
+    """
+    base_name = Tensor.getBaseName(name)
     group = self.group(name)
-    return self[baseName][group] if group is not tuple() else self[baseName]
+    return self[base_name][group] if group is not tuple() else self[base_name]
+
 
   def containsName(self, name):
+    """Checks whether there is a tensor with the given name by the user
+
+    Args:
+      name (str): a tensor name
+
+    Returns:
+      bool: True if there is a tensor with such a name. Otherwise, False
+
+    Raises:
+      ValueError: if a tensor name is invalid according to the Tensor class specification
+
+    Examples:
+      >>> names = ['A_00', 'A_01', 'B_0', 'C']
+      >>> shapes = [(4,4), (3,4), (3,2), (3,2)]
+      >>> tensors = {}
+      >>> for name, shape in zip(names, shapes):
+      ...     tensors[name] = Tensor(name, shape)
+      ...
+      >>> from yateto.input import __createCollection
+      >>> collection = __createCollection(Tensors)
+      >>> collection.containsName('C')
+      True
+      >>> collection.containsName('A_00')
+      True
+      >>> collection.containsName('X')
+      False
+    """
+
     if not Tensor.isValidName(name):
       raise ValueError('Invalid name: {}'.format(name))
 
-    baseName = Tensor.getBaseName(name)
+    base_name = Tensor.getBaseName(name)
     group = self.group(name)
-    return baseName in self and (group is tuple() or group in self[baseName])
+    return base_name in self and (group is tuple() or group in self[base_name])

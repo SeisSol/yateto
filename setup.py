60c424721825abacdd6fa95bfeab637d7a5827a8
ravil <ravil.dorozhinskii@tum.de>
2020-05-15 18:32:45 +0200

    Added Eigen to CTest and installation
    * Added Eigen to testing with CTest
    * Added setup.py for a local installation with pip
    * Added an example of the DSL to README

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+install_requires = ['numpy', 'scipy']
+
+setuptools.setup(
+    name="yateto",
+    version="0.1.0",
+    license="MIT",
+    author="Carsten Uphoff",
+    author_email="uphoff@in.tum.com",
+    description="A tensor toolbox for discontinuous Galerkin methods",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["yateto"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD 3-Clause License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=install_requires,
+)
--- tests/generator.py
+++ tests/generator.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-import sys
-sys.path.append('..')
-
 import os, errno
 import argparse
 import importlib.util

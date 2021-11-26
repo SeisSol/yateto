893091b5d4a11f5b848fb2bdc8d0e18d54079e4b
Lukas Krenz <lukas@krenz.land>
2021-11-26 16:06:49 +0100

    Fix CI by adding definition

--- yateto/codegen/test_framework.py
+++ yateto/codegen/test_framework.py
@@ -27,6 +27,10 @@ class TestFramework(ABC):
         cpp.include(kernelsInclude)
         cpp.include(initInclude)
         cpp.include('yateto.h')
+        with cpp.PPIfndef('NDEBUG'):
+            with cpp.PPIfndef('YATETO_TESTING_NO_FLOP_COUNTER'):
+                cpp('long long libxsmm_num_total_flops = 0;')
+                cpp('long long pspamm_num_total_flops = 0;')
 
 class CxxTest(TestFramework):
     TEST_CLASS = 'KernelTestSuite'
